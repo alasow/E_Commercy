0745c1534a279ff190ae06e21bae8048ea94b3ea
Amiin <koofirow17@gmail.com>
2025-03-05 11:14:17 +0300

    v3

--- ECommerceApp.java
+++ ECommerceApp.java
@@ -1,6 +1,6 @@
 public class ECommerceApp {
     public static void main(String[] args) {
         // Run the application
-        System.out.println("E Commerce Project...Loading UPdated By Students V2");
+        System.out.println("E Commerce Project...Loading UPdated By Students V3");
     }
 }
