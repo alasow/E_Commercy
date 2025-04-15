9c65b39c22f722ef3d7773ab1194ed7cd3b3a4ff
alasow <calasow029@hotmail.com>
2025-04-15 22:10:19 +0300

    Update ECommerceApp.java

--- ECommerceApp.java
+++ ECommerceApp.java
@@ -9,7 +9,7 @@ import java.util.*;
  class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/ecommerce_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "Mc@am0615207039"; // Or your actual password
+    private static final String PASSWORD = "Mcxxxxxxxx"; // Or your actual password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
